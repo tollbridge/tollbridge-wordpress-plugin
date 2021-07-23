7c6503bfbffc60925f9456432f5034058b68ad41
George Dumitrescu <george@dumitres.co>
2021-07-23 17:43:34 +0300

    added amp-access integration

---
+++ views/amp/view.php
@@ -0,0 +1,15 @@
+<?php
+
+$manager = new \Tollbridge\Paywall\Manager();
+
+if ($manager->allAccountSettingsAreEntered()) {
+    global $post;
+
+    $plans = $this->manager->getApplicablePlans($post);
+
+    require_once plugin_dir_path(dirname(__FILE__)) . '/../views/amp/leaky.php';
+    require_once plugin_dir_path(dirname(__FILE__)) . '/../views/amp/fullscreen.php';
+    require_once plugin_dir_path(dirname(__FILE__)) . '/../views/amp/slideup.php';
+    require_once plugin_dir_path(dirname(__FILE__)) . '/../views/amp/inline.php';
+    require_once plugin_dir_path(dirname(__FILE__)) . '/../views/amp/container.php';
+}
